d2bd8d92fb21df265af7805c00586740a4daa1c2
Vishwa Maduranga <91451460+Vishwa7898@users.noreply.github.com>
2026-04-30 21:49:43 +0530

    Add EnvTest.java for testing environment properties

---
+++ backend/src/test/java/EnvTest.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class EnvTest {
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    void printMongoUri() {
+        System.out.println("==================================================");
+        System.out.println("MONGO URI: " + env.getProperty("spring.data.mongodb.uri"));
+        String clientId = env.getProperty("spring.security.oauth2.client.registration.google.client-id");
+        if (clientId != null) {
+            System.out.println("CLIENT ID: " + clientId);
+        }
+        System.out.println("==================================================");
+    }
+}
